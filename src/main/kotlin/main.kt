bf432c3d80203d34998040c62315db0a1ac54bff
ericsousa-c <eric0611w@gmail.comgit config --global user.email eric0611w@gmail.com>
2022-12-10 16:15:15 -0300

    First commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    valores()
+    save()
+}
+
+fun valores(value: Any = 0): Unit {
+    val funUnit = "Nao retorna nada"
+    print(value)
+}
+
+fun save(): Nothing {
+    TODO("Ainda em execução")
+}
